faddb98e096e44dbda8709e878748260977f926c
Fabio Forni <development@redaril.me>
2023-07-06 16:04:25 +0200

    Correctly parse multicharacter tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,69 +4,140 @@ use std::iter;
 use std::slice;
 use std::str::{self, FromStr};
 
-#[derive(strum::Display, strum::EnumString, Debug, PartialEq, Eq)]
-#[strum(serialize_all = "UPPERCASE")]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Token {
-    #[strum(serialize = "")]
-    EOF,
-
     Identifier(String),
-    Integer(String),
+    Integer(i64),
 
-    #[strum(serialize = "=")]
     Assign,
-    #[strum(serialize = "+")]
     Plus,
 
-    #[strum(serialize = ",")]
     Comma,
-    #[strum(serialize = ";")]
     Semicolon,
 
-    #[strum(serialize = "(")]
     LeftParen,
-    #[strum(serialize = ")")]
     RightParen,
-    #[strum(serialize = "{")]
     LeftBrace,
-    #[strum(serialize = "}")]
     RightBrace,
 
-    #[strum(serialize = "fn")]
     Function,
-    #[strum(serialize = "let")]
     Let,
+}
+
+impl Token {
+    fn from_char(ch: char) -> Option<Self> {
+        match ch {
+            '=' => Some(Self::Assign),
+            '+' => Some(Self::Plus),
+            ',' => Some(Self::Comma),
+            ';' => Some(Self::Semicolon),
+            '(' => Some(Self::LeftParen),
+            ')' => Some(Self::RightParen),
+            '{' => Some(Self::LeftBrace),
+            '}' => Some(Self::RightBrace),
+            _ => None,
+        }
+    }
+}
+
+impl FromStr for Token {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.is_empty() {
+            return Err(Error::TokenError(String::new()));
+        }
+        let first = s.as_bytes()[0] as char;
+        if s.len() == 1 {
+            if let Some(tok) = Self::from_char(first) {
+                return Ok(tok);
+            }
+        }
+        match s {
+            "fn" => Ok(Self::Function),
+            "let" => Ok(Self::Let),
+            _ => {
+                if first.is_ascii_digit() {
+                    return i64::from_str(s)
+                        .map(Self::Integer)
+                        .map_err(|_| Error::TokenError(String::from(s)));
+                }
+                Ok(Self::Identifier(s.to_string()))
+            }
+        }
+    }
 }
 
 pub struct Lexer<R> {
     input: io::BufReader<R>,
+    token_buf: String,
+    /// The latest character read but not yet parsed.
+    latest_char: Option<char>,
 }
 
-impl<T: io::Read> Lexer<T> {
-    pub fn new(inp: T) -> Self {
+impl<R: io::Read> Lexer<R> {
+    pub fn new(inp: R) -> Self {
         Self {
             input: io::BufReader::new(inp),
+            token_buf: String::with_capacity(32),
+            latest_char: None,
         }
     }
 
-    fn read_char(&mut self) -> Result<u8, Error> {
-        let mut ch: u8 = 0;
+    /// Returns the next character.
+    fn read_char(&mut self) -> Result<char, Error> {
+        let mut byte: u8 = 0;
         self.input
-            .read_exact(slice::from_mut(&mut ch))
+            .read_exact(slice::from_mut(&mut byte))
             .map_err(Error::from)?;
-        Ok(ch)
+        char::from_u32(byte as u32).ok_or_else(|| Error::TokenError(format!("{:x}", byte)))
+    }
+
+    fn read_nonwhitespace_char(&mut self) -> Result<char, Error> {
+        loop {
+            let ch = self.read_char()?;
+            if !ch.is_ascii_whitespace() {
+                return Ok(ch);
+            }
+        }
     }
 
     fn read_token(&mut self) -> Result<Token, Error> {
-        let s = &[self.read_char()?];
-        str::from_utf8(s)
-            .ok()
-            .and_then(|s| Token::from_str(s).ok())
-            .ok_or_else(|| Error::TokenError(format!("{:x}", s[0])))
+        let ch = match self.latest_char.filter(|c| !c.is_ascii_whitespace()) {
+            Some(c) => c,
+            None => self.read_nonwhitespace_char()?,
+        };
+        if let Some(tok) = Token::from_char(ch) {
+            self.latest_char = None;
+            return Ok(tok);
+        }
+        self.token_buf.clear();
+        self.token_buf.push(ch);
+        if is_identifier_char(ch) {
+            self.fill_buffer_until(is_identifier_char)?;
+        } else if ch.is_ascii_digit() {
+            self.fill_buffer_until(|ch| ch.is_ascii_digit())?;
+        }
+        Token::from_str(&self.token_buf).map_err(|_| Error::TokenError(self.token_buf.clone()))
+    }
+
+    fn fill_buffer_until<F>(&mut self, cond: F) -> Result<(), Error>
+    where
+        F: Fn(char) -> bool,
+    {
+        loop {
+            let ch = self.read_char()?;
+            self.latest_char = Some(ch);
+            if !cond(ch) {
+                break;
+            }
+            self.token_buf.push(ch);
+        }
+        Ok(())
     }
 }
 
-impl<T: io::Read> iter::Iterator for Lexer<T> {
+impl<R: io::Read> iter::Iterator for Lexer<R> {
     type Item = Result<Token, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -89,6 +160,10 @@ pub enum Error {
     #[error("I/O error")]
     IO(#[from] io::Error),
 
-    #[error("unknown token string {0}")]
+    #[error("invalid token \"{0}\"")]
     TokenError(String),
+}
+
+fn is_identifier_char(ch: char) -> bool {
+    ch.is_ascii_alphabetic() || ch == '_'
 }
